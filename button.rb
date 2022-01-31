86bb4e34c290e0bddfaffe8f4d9fa5307cc1f581
Aaron Elkins <threcius@yahoo.com>
2022-01-31 15:26:52 +0800

    Add Button class

---
+++ button.rb
@@ -0,0 +1,45 @@
+require 'gtk3'
+
+def roundrect(context, x, y, width, height, radius)
+  context.arc(x + radius, y + radius, radius,
+              Math::PI, 3 * Math::PI / 2)
+  context.arc(x + width - radius, y + radius, radius,
+              3 * Math::PI / 2, 0)
+  context.arc(x + width - radius, y + height - radius,
+              radius, 0, Math::PI / 2)
+  context.arc(x + radius, y + height - radius, radius,
+              Math::PI / 2, Math::PI)
+  context.close_path()
+end
+
+class Button < Gtk::DrawingArea
+  def initialize(text)
+    super()
+    add_events(Gdk::EventMask::BUTTON_PRESS_MASK)
+    add_events(Gdk::EventMask::BUTTON_RELEASE_MASK)
+    add_events(Gdk::EventMask::POINTER_MOTION_MASK)
+    signal_connect "draw" do |widget, context|
+      do_drawing(widget, context)
+    end
+    @width = 0
+    @height = 0
+    @text = text
+  end
+
+  def set_size(width, height)
+    self.set_size_request(width, height)
+    @width = width
+    @height = height
+  end
+
+  def do_drawing(widget, context)
+    puts "do_drawing"
+    context.set_source_rgb(0.55, 0.95, 0.26)
+    roundrect(context, 0, 0, @width, @height, 5)
+    context.fill()
+    context.move_to(7, 18)
+    context.set_source_rgb(1.0, 1.0, 1.0)
+    context.set_font_size(14)
+    context.show_text(@text)
+  end
+end
--- notify.rb
+++ notify.rb
@@ -1,24 +1,11 @@
 require 'gtk3'
 require 'optparse'
+require './button'
 
 options = {}
 OptionParser.new do |opt|
   opt.on('--mail_count MAILCOUNT') { |o| options[:mail_count] = o }
 end.parse!
-
-def roundrect(context, x, y, width, height, radius)
-    context.arc(x + radius, y + radius, radius,
-                Math::PI, 3 * Math::PI / 2)
-    context.arc(x + width - radius, y + radius, radius,
-                3 * Math::PI / 2, 0)
-    context.arc(x + width - radius, y + height - radius,
-                radius, 0, Math::PI / 2)
-    context.arc(x + radius, y + height - radius, radius,
-                Math::PI / 2, Math::PI)
-    context.close_path()
-end
-
-
 
 w = 350
 h = 130
@@ -54,12 +41,13 @@ text = "You have %d new mail!" % options[:mail_count]
 label = Gtk::Label.new(text)
 label.set_markup("<big>%s</big>" % text)
 fixed.put(label, 100, 30)
-window.add(fixed)
 
 # Add button
-text = "You have %d new mail!" % options[:mail_count]
-button = Gtk::Button.new(:label => text)
-fixed << button
+button = Button.new("Okay!")
+button.set_size(50, 26)
+fixed.put(button, 150, 90)
+
+window.add(fixed)
 
 window.signal_connect("delete-event") { |_widget| Gtk.main_quit }
 window.show_all
