26967f404c95304901d8c114592f4a8d708809d9
Aaron Elkins <threcius@yahoo.com>
2022-01-31 16:32:09 +0800

    Update notify.rb

--- mail.rb
+++ mail.rb
@@ -2,12 +2,16 @@ require "gtk3"
 require 'thread'
 
 timer = Thread.new do
-  Ten_Minutes = 4 #60 * 10
+  Ten_Minutes = 60 #60 * 10
   while true
-    unread = `./unread.oas`
-    if unread.to_i > 0
-      cmd = "ruby notify.rb --mail_count=" + unread
-      `#{cmd}`
+    running = `ps | grep "ruby notify.rb"`
+    print running
+    if running.count("ruby notify.rb") < 2
+      unread = `./unread.oas`
+      if unread.to_i > 0
+        cmd = "ruby notify.rb --mail_count=" + unread
+        `#{cmd}`
+      end
     end
     sleep Ten_Minutes
   end
